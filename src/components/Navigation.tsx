7f2ab119d9f37cdb5c583f0b52356b0e1b1e34e1
John Hernandez <johnhdz.160@gmail.com>
2023-04-22 14:01:43 -0500

    front: navigation stylized

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
-import type { NavigationLink } from "../types";
+import React from "react";
 import { NavLink } from "react-router-dom";
+import { MdOutlineInventory } from 'react-icons/md'
 import links from "../data/navigationLinks";
 
-const Navigation = () => {
+interface NavigationProps {
+  showNavigation: boolean
+}
 
+const LINK_STYLE =
+  "w-full h-full p-3 px-6 hover:bg-[#1D1B31] hover:text-slate-300 transition-colors";
+
+const Navigation = ({ showNavigation }: NavigationProps) => {
   return (
-    <div>
-      <nav>
-        <ul>
+    <div className={`bg-[#11111D] text-[#FFFFFF] fixed transition-transform h-screen py-6 ${showNavigation ? 'w-[250px]': 'w-0 hidden'}`}>
+      <nav className="flex flex-col gap-6 min-h-[400px] h-full overflow-y-auto">
+        <h2 className="text-center font-normal text-2xl">Navegación</h2>
+        <ul className="flex flex-col gap-4 text-md font-normal text-[#a3a3a3]">
+          <li className="flex">
+            <button className={`${LINK_STYLE} text-left`}>Inventarios</button>
+          </li>
           {links.map((link, idx) => (
-            <li key={idx}>
-              <NavLink className={(navData) => (navData.isActive ? 'bg-red-300': 'bg-transparent')} to={link.route}>{link.name}</NavLink>
+            <li key={idx} className="flex">
+              <NavLink className={LINK_STYLE} to={link.route}>
+                {link.icon}
+                {link.name}
+              </NavLink>
             </li>
           ))}
         </ul>
