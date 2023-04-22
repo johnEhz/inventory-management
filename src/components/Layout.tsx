75b8521d7f12b5764068eb82afb9cb7f3e9fb309
John Hernandez <johnhdz.160@gmail.com>
2023-04-22 13:57:46 -0500

    front: Layout component stylized

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
@@ -8,11 +8,18 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const [showNavigation, setShowNavigation] = useState(false);
+  const toggleShowNavigation = () => {
+    setShowNavigation(!showNavigation);
+  };
   return (
     <>
-      <Header />
-      <Navigation />
-      <main>{children}</main>
+      <Header
+        toggleShowNavigation={toggleShowNavigation}
+        showNavigation={showNavigation}
+      />
+      <Navigation showNavigation={showNavigation} />
+      <main className="bg-gray-50 w-full h-screen min-h-[700px]">{children}</main>
       <Footer />
     </>
   );
