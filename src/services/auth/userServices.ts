bc75742756ee69b51daf16dc2eb6474ff65a92e5
John Hernandez <johnhdz.160@gmail.com>
2023-04-22 14:04:02 -0500

    backend: auth services added

---
+++ src/services/auth/userServices.ts
@@ -0,0 +1,44 @@
+import { UserRegistration, UserLogin } from '../../types'
+import axios from 'axios'
+
+const REGISTER_ROUTE = 'http://localhost:4000/api/auth/register'
+const LOGIN_ROUTE = 'http://localhost:4000/api/auth/login'
+
+export const createUser = async (userRegister: UserRegistration) => {
+    try {
+        const { name, email, password } = userRegister
+        const response = await axios.post(REGISTER_ROUTE, {
+            name,
+            email,
+            password
+        })
+        return response
+    } catch (error: any) {
+        if (error.response && (error.response.status === 409 || error.response.status === 400 || error.response.status === 500)) {
+            console.error(error.response.data)
+            throw new Error(error.response.data.message)
+        }
+        else {
+            throw new Error('Error Desconocido')
+        }
+    }
+}
+
+export const loginUser = async (userRegister: UserLogin) => {
+    try {
+        const { email, password } = userRegister
+        const response = await axios.post(LOGIN_ROUTE, {
+            email,
+            password
+        })
+        return response
+    } catch (error: any) {
+        if (error.response && (error.response.status === 400 || error.response.status === 401)) {
+            console.error(error.response.data)
+            throw new Error(error.response.data.message)
+        }
+        else {
+            throw new Error('Error Desconocido')
+        }
+    }
+}
