42ccf61e5dba02a984e5da26aa76f632f2959626
John Hernandez <johnhdz.160@gmail.com>
2023-04-22 13:59:06 -0500

    front: register and login handled

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,40 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdEmail, MdKey } from "react-icons/md";
 import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
+
+//Services
+import { loginUser } from "../services/auth/userServices";
+
+//Types - Interfaces
+import { UserLogin } from "../types";
 
 const INPUT_STYLE =
   "w-full ml-1 p-1 px-2 text-md outline-0 bg-transparent rounded";
 
+const LOGIN_INITIAL_STATE: UserLogin = {
+  email: "",
+  password: "",
+};
+
 const Login = () => {
+  const [userLogin, setUserLogin] = useState<UserLogin>(LOGIN_INITIAL_STATE);
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserLogin((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleLoginUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      const result = await loginUser(userLogin);
+      const { token, name } = result.data;
+      console.log(result.data)
+      localStorage.setItem("Authorization", token);
+      toast.success(`Bienvenido ${name}`);
+    } catch (error: any) {
+      toast.error(`${error}`);
+      return;
+    }
+  };
+
   return (
-    <div className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 w-full h-screen min-h-[600px] flex flex-col justify-center items-center p-2 gap-5">
-      <h1 className="text-gray-50 font-bold text-5xl welcome-message">
-        Bienvenido!
-      </h1>
-      <form className="bg-gray-50 max-w-[480px] w-full flex flex-col gap-7 py-10 px-2 sm:px-4 m-5 text-[#11111D] rounded">
+    <div className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 w-full h-screen min-h-[650px] flex flex-col justify-center items-center p-2 gap-5">
+      <h1 className="text-gray-50 font-bold text-5xl message">Bienvenido!</h1>
+      <form
+        onSubmit={handleLoginUser}
+        className="bg-gray-50 max-w-[480px] w-full flex flex-col gap-7 py-10 px-2 sm:px-4 m-5 text-[#11111D] rounded"
+      >
         <h2 className="text-center text-3xl font-bold">Login</h2>
         <ul className="flex flex-col gap-4">
-          <li className="flex flex-row items-center p-2 border">
+          <li className="flex flex-row items-center p-2 border rounded">
             <MdEmail className="text-[#9B9B98]" size={20} />
             <input
               className={INPUT_STYLE}
               type="email"
+              name="email"
+              value={userLogin.email}
+              onChange={handleChangeInput}
               placeholder="Correo Electronico"
               required
             />
           </li>
-          <li className="flex flex-row items-center p-2 border">
+          <li className="flex flex-row items-center p-2 border rounded">
             <MdKey className="text-[#9B9B98]" size={20} />
             <input
               className={INPUT_STYLE}
               type="password"
+              name="password"
+              value={userLogin.password}
+              onChange={handleChangeInput}
               placeholder="Contraseña"
               required
             />
           </li>
         </ul>
         <button
-          className="bg-[#11111D] text-gray-50 py-[12px] font-normal rounded"
+          className="bg-[#6184FF] text-gray-50 py-[12px] font-normal rounded hover:bg-[#4565db] transition-colors"
           type="submit"
         >
           Login
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,56 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdEmail, MdKey } from "react-icons/md";
-import { RiPencilFill } from "react-icons/ri"
+import { RiPencilFill } from "react-icons/ri";
 import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
+
+//Services
+import { createUser } from "../services/auth/userServices";
+
+//Utils
+import { checkConfirmPassword } from "../utils/checkConfirmPassword";
+
+//Types - Interfaces
+import { UserRegistration } from "../types";
 
 const INPUT_STYLE =
   "w-full ml-1 p-1 px-2 text-md outline-0 bg-transparent rounded";
 
+const REGISTER_INITIAL_STATE: UserRegistration = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const Register = () => {
+  const [userRegistration, setUserRegistration] = useState<UserRegistration>(
+    REGISTER_INITIAL_STATE
+  );
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserRegistration((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleRegisterUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      const { password, confirmPassword } = userRegistration;
+      if (!checkConfirmPassword(password, confirmPassword)) {
+        throw new Error("Las contraseñas ingresadas no coninciden");
+      }
+      const result = await createUser(userRegistration);
+      const { token, name } = result.data;
+      localStorage.setItem("Authorization", token);
+      toast.success(`Usuario ${name} registrado exitosamente`);
+    } catch (error: any) {
+      toast.error(`${error}`);
+      return;
+    }
+  };
+
   return (
-    <div className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 w-full h-screen min-h-[650px] flex flex-col justify-center items-center p-2 gap-5">
-      <form className="bg-gray-50 max-w-[480px] w-full flex flex-col gap-7 py-10 px-2 sm:px-4 m-5 text-[#11111D] rounded">
+    <div className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 w-full h-screen min-h-[700px] flex flex-col justify-center items-center p-2 gap-5">
+      <h1 className="text-gray-50 font-bold text-5xl message">Crear cuenta</h1>
+      <form
+        onSubmit={handleRegisterUser}
+        className="bg-gray-50 max-w-[480px] w-full flex flex-col gap-7 py-10 px-2 sm:px-4 m-5 text-[#11111D] rounded"
+      >
         <h2 className="text-center text-3xl font-bold">Registrarse</h2>
         <ul className="flex flex-col gap-4">
-        <li className="flex flex-row items-center p-2 border">
+          <li className="flex flex-row items-center p-2 border rounded">
             <RiPencilFill className="text-[#9B9B98]" size={20} />
             <input
               className={INPUT_STYLE}
               type="text"
+              name="name"
+              value={userRegistration.name}
               placeholder="Nombre Completo"
+              onChange={handleChangeInput}
               required
             />
           </li>
-          <li className="flex flex-row items-center p-2 border">
+          <li className="flex flex-row items-center p-2 border rounded">
             <MdEmail className="text-[#9B9B98]" size={20} />
             <input
               className={INPUT_STYLE}
               type="email"
+              name="email"
+              value={userRegistration.email}
               placeholder="Correo Electronico"
+              onChange={handleChangeInput}
               required
             />
           </li>
-          <li className="flex flex-row items-center p-2 border">
+          <li className="flex flex-row items-center p-2 border rounded">
             <MdKey className="text-[#9B9B98]" size={20} />
             <input
               className={INPUT_STYLE}
               type="password"
+              name="password"
+              value={userRegistration.password}
               placeholder="Contraseña"
+              onChange={handleChangeInput}
               required
             />
           </li>
-          <li className="flex flex-row items-center p-2 border">
+          <li className="flex flex-row items-center p-2 border rounded">
             <MdKey className="text-[#9B9B98]" size={20} />
             <input
               className={INPUT_STYLE}
               type="password"
+              name="confirmPassword"
+              value={userRegistration.confirmPassword}
               placeholder="Confirmar Contraseña"
+              onChange={handleChangeInput}
               required
             />
           </li>
         </ul>
         <button
-          className="bg-[#11111D] text-gray-50 py-[12px] font-normal rounded"
+          className="bg-[#6184FF] text-gray-50 py-[12px] font-normal rounded hover:bg-[#4565db] transition-colors"
           type="submit"
         >
           Registrarse
