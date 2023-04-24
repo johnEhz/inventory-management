dc4ca316220a5a31ee996a67fb74b75515ff7422
John Hernandez <johnhdz.160@gmail.com>
2023-04-24 16:28:01 -0500

    types: new types and interfaces added

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,7 @@
 export interface NavigationLink {
     name: string
     route: string
-    icon: IconType
+    Icon: IconType
 }
 
 
@@ -13,3 +13,57 @@ export interface UserRegistration {
 }
 
 export type UserLogin = Omit<UserRegistration, 'name' | 'confirmPassword'>
+
+export interface AuthUser extends Omit<UserRegistration, 'password' | 'confirmPassword'> {
+    token?: string
+}
+
+export interface AuthContextI {
+    user?: AuthUser,
+    login?: (data: AuthUser) => void
+    logout?: () => void
+}
+
+export interface Brand {
+    _id: string
+    _author: string
+    name: string
+    createdAt: string
+    updatedAt: string
+}
+
+export interface Provider {
+    _id: string
+    _author: string
+    name: string
+    createdAt: string
+    updatedAt: string
+}
+
+export interface Product {
+    _reference: string
+    name: string
+    description: string
+    brand: Brand
+    provider: Provider
+    quantity: number
+    price: number
+    createdAt: string
+    updatedAt: string
+    invest: number
+
+}
+
+export interface Inventory {
+    _id: string
+    _author: string
+    name: string
+    description: string
+    items: Product[]
+    createdAt: string
+    updatedAt: string
+}
+
+export type InventoryRoute =  Omit<Inventory, '_author' | 'description' | 'items' | 'updatedAt'>
+
+export type InventoryCreation = Omit<Inventory, '_id' | '_author' | 'items' | 'updatedAt' | 'createdAt'>
